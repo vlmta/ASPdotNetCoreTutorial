9d346cad086b90c84ac08b989fed8b0ed14b409a
vlmta <vlmtam@outlook.com>
2020-08-09 09:30:30 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPdotNetCoreTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
