4a475d5a1ed75bb4e0ab256fff2e2e34ea0ecfaf
tam.vo <tam.vo@pap-tech.com>
2020-08-10 15:11:51 +0700

    add database context class in Data Folder

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,20 @@
+﻿using ASPdotNetCoreTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPdotNetCoreTutorial.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
