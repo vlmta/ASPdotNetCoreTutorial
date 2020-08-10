16b7b1fec009293c51fb4e5c98e3332141715fec
tam.vo <tam.vo@pap-tech.com>
2020-08-10 09:17:38 +0700

    add HelloWorld Controller.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPdotNetCoreTutorial.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        /*
+          Index is the default method that will be called on a controller 
+        if a method name isn't explicitly specified.
+        format:
+            [Controller]/[ActionName]/[Parameters]
+         */
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
