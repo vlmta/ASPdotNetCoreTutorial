85d85b7f7b7a63f12850ab54d094c033cbea9582
tam.vo <tam.vo@pap-tech.com>
2020-08-11 10:51:45 +0700

    add data annotations for releasedate and price

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,15 @@ namespace ASPdotNetCoreTutorial.Models
 {
     public class Movie
     {
-
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name ="Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName ="(18, 2)")]
         public decimal Price { get; set; }
 
     }
