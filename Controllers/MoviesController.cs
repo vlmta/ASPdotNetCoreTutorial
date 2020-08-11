80387cf05e62df702dcd22e2ade360ee5be5bac5
tam.vo <tam.vo@pap-tech.com>
2020-08-11 11:37:22 +0700

    Modify action method with search.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,8 +20,15 @@ namespace ASPdotNetCoreTutorial.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
+            var movies =from m 
+                       in  _context.Movie 
+                       select m;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
             return View(await _context.Movie.ToListAsync());
         }
 
