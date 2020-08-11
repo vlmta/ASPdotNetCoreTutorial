038fab2da9623ade21077f8dff45e4d1747deaac
tam.vo <tam.vo@pap-tech.com>
2020-08-11 15:38:12 +0700

    add Rating int Bind[ ]

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -60,7 +60,7 @@ namespace ASPdotNetCoreTutorial.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -92,7 +92,7 @@ namespace ASPdotNetCoreTutorial.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (id != movie.Id)
             {
